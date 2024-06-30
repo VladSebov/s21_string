5117a7064f0b3d3ad93b1c0fbe09ca09ea9443b0
Fruet <fruitbue@student.21-school.ru>
2024-06-30 22:57:17 +0300

    strcspn,strncpy

---
+++ s21_strcspn.c
@@ -0,0 +1,13 @@
+#include "s21_string+.h"
+
+s21_size_t s21_strcspn(const char *str1, const char *str2) {
+s21_size_t i = 0;
+    while (i < s21_strlen(str1)) {
+        if (s21_memchr(str2, *(str1 + i), s21_strlen(str2)) == NULL) {
+            i++;
+        } else {
+            break;
+        }
+    }
+    return (i);
+}
