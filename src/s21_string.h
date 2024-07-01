a1d4b51b42922bcdee41d116a15066f9b4c92088
Sharlene Nidorina <sharlenn@vo-c5.yar.21-school.ru>
2024-07-01 20:41:17 +0300

    Added correct version of s21_NULL

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,14 +1,13 @@
 #ifndef S21_STRING_H
 #define S21_STRING_H
 
-#define s21_NULL 0
+#define s21_NULL ((void*)0)
 
 typedef long unsigned int s21_size_t;
 
 void *s21_memchr(const void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
-void *s21_memmove(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
 char *s21_strchr(const char *str, int c);
