1d823eeb44791a15ac2c0c198fb86b50bf56788c
Sharlene Nidorina <sharlenn@vo-c5.yar.21-school.ru>
2024-07-01 20:42:12 +0300

    Improved strrchr function

--- src/s21_strrchr.c
+++ src/s21_strrchr.c
@@ -1,11 +1,10 @@
 #include "s21_string.h"
 
 char *s21_strrchr(const char *str, int c) {
-  char *value = "";
-  str = str + 1;
-  /* for(s21_size_t i = 0; str[i]!='\0'; i++) {
-      if (str[i] == c) last_occurance = i;
-  } */
-  c++;
-  return value;
+  char *last_occurance = s21_NULL;
+  for (s21_size_t i = 0; i <= s21_strlen(str); i++) {
+    if (str[i] == c)
+      last_occurance = (char *)str + i;
+  }
+  return last_occurance;
 }
