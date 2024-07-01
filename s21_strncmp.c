eb149919f4c210e7f11bc5c5b989d7a779620f06
jeneekar <tarasova.xeniya@gmail.com>
2024-07-01 21:27:44 +0300

    add: s21_strcnmp.c

---
+++ s21_strncmp.c
@@ -0,0 +1,13 @@
+#include "s21_string.h"
+#include <stdio.h>
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n)
+{
+   // пока символы равны и n  не кончилось идем по строке
+    while (*str1 == *str2 && --n){
+      str1++; str2++;
+      
+    
+    }
+   
+   return *str1-*str2;
+}
